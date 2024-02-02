044c6918aaab72d996559ef9658c23f912d403a4
David MARTIN <david@dmidz.com>
2024-02-02 11:49:30 +0100

    setup lib build ( pck vite-plugin-dts, etc... )

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,44 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'node:url';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'node:path';
+
+const res = ( path: string ) => resolve( __dirname, path );
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': fileURLToPath( new URL( './src', import.meta.url ) )
+      '@': resolve( __dirname, 'src' ),
     }
   },
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    dts({// gently generates *.d.ts files at build :)
+      pathsToAliases: false,
+      rollupTypes: true,
+      include: [ 'src/lib/**/*.ts' ],
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: {
+        index: res( 'src/lib/index.ts' ),
+        // utils: res( 'src/utils/index.ts' ),
+      },
+      name: 'TicksChart',
+      formats: [ 'es' ],
+      // fileName: 'tickschart',
+    },
+    rollupOptions: {
+      external: [ 'vue' ],// make sure to externalize deps that shouldn't be bundled into your library
+      output: {
+        globals: {// Provide global variables to use in the UMD build for externalized deps
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+});
   

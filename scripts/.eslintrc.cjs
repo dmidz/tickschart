ca1d3b6c8bcadc9ebc6c1439c1b6c67b1b080984
David MARTIN <david@dmidz.com>
2024-02-05 21:11:37 +0100

    install packages for release.js script v1 (#4)

---
+++ scripts/.eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+}
---
+++ scripts/release.js
@@ -0,0 +1,211 @@
+
+/*//// heavily copied from Vue core release script https://github.com/vuejs/core/blob/main/scripts/release.js */
+
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { createRequire } from 'node:module';
+import minimist from 'minimist';
+import { execa } from 'execa';
+import semver from 'semver';
+import pico from 'picocolors';
+import enquirer from 'enquirer';
+const { prompt } = enquirer;
+
+//___
+const __dirname = path.dirname( fileURLToPath( import.meta.url ) );
+const currentVersion = createRequire( import.meta.url )( '../package.json' ).version;
+
+const args = minimist( process.argv.slice( 2 ), {
+	alias: {
+		skipBuild: 'skip-build',
+		skipTests: 'skip-tests',
+		skipGit: 'skip-git',
+		skipPrompts: 'skip-prompts',
+	},
+} );
+
+const preId = args.preid || semver.prerelease( currentVersion )?.[ 0 ];
+
+/** @type {ReadonlyArray<import('semver').ReleaseType>} */
+const versionIncrements = [
+	'patch',
+	'minor',
+	'major',
+	...( preId
+		? /** @type {const} */ [ 'prepatch', 'preminor', 'premajor', 'prerelease' ]
+		: [] ),
+];
+
+let versionUpdated = false;
+
+//___
+async function main ( options = {} ){
+	let targetVersion = options._[ 0 ];
+	
+	if( !targetVersion ){
+		// no explicit version, offer suggestions
+		/** @type {{ release: string }} */
+		const { release } = await prompt( {
+			type: 'select',
+			name: 'release',
+			message: 'Select release type',
+			choices: versionIncrements
+				.map( i => `${ i } (${ inc( i ) })` )
+				// .concat( [ 'custom' ] ),
+		} );
+
+		if( release === 'custom' ){
+			/** @type {{ version: string }} */
+			const result = await prompt( {
+				type: 'input',
+				name: 'version',
+				message: 'Input custom version',
+				initial: currentVersion,
+			} );
+			targetVersion = result.version;
+		} else {
+			targetVersion = release.match( /\((.*)\)/ )?.[ 1 ] ?? '';
+		}
+
+		if( !semver.valid( targetVersion ) ){
+			throw new Error( `invalid target version: ${ targetVersion }` );
+		}
+
+		/** @type {{ yes: boolean }} */
+		// const { yes: confirmRelease } = await prompt( {
+		// 	type: 'confirm',
+		// 	name: 'yes',
+		// 	message: `Confirm release v${ targetVersion } ?`,
+		// } )
+		//
+		// if( !confirmRelease ){
+		// 	return;
+		// }
+
+		/*__ Step: sync local develop from origin & derive new master */
+		if( !options.dry ){
+			step( 'Sync local develop...' );
+			await run( 'git', [ 'fetch', 'origin', 'develop:develop', '--recurse-submodules=no', '--prune' ] );
+			await run( 'git', [ 'checkout', 'develop' ] );
+		}
+
+		/*__ Step: run tests */
+		// step( 'Running tests...' );
+
+		/*__ Step: update package version */
+		step( 'Update version...' );
+		updatePackageVersion( targetVersion );
+		versionUpdated = true;
+
+		/*__ Step: build */
+		step( 'Building...' );
+		await run( 'npm', [ 'run', 'build' ] );
+
+		/*__ Step: generate changelog */
+		step( 'Generating changelog...' );
+		await run( `npm`, [ 'run', 'changelog' ] );
+		
+		/*__ Step: git add & commit */
+		if( !options.dry ){
+			step( 'Commiting changes...' );
+			const { stdout } = await run( 'git', [ 'diff' ], { stdio: 'pipe' } );
+			if( stdout ){
+				await run( 'git', [ 'add', '-A' ] );
+				await run( 'git', [ 'commit', '-m', `release: v${ targetVersion }` ] );
+			} else {
+				console.log( 'No changes to commit.' );
+			}
+		}
+
+		/*__ Step: update local master from develop */
+		if( !options.dry ){
+			step( 'Merge develop into master...' );
+			await run( 'git', [ 'fetch', 'origin', 'master:master', '--recurse-submodules=no', '--prune' ] );
+			await run( 'git', [ 'checkout', 'master' ] );
+			await run( 'git', [ 'merge', 'develop' ] );
+			//__ create release branch ?
+			// await run( 'git', [ 'checkout','-b',`release/v${targetVersion}` ] );
+		}
+
+		/*__ Step: npm publish */
+		step( 'Publishing...' );
+		const publishFlags = [];
+		if( options.dry ){
+			publishFlags.push( '--dry-run' );
+		}
+		await publish( publishFlags );
+
+		/*__ Step: git push & push tags */
+		if( !options.dry ){
+			step( 'Pushing commits & tag...' );
+			await run( 'git', [ 'push', 'origin', 'master' ] );
+			await run( 'git', [ 'tag', `v${ targetVersion }` ] );
+			await run( 'git', [ 'push', 'origin', 'master', `refs/tags/v${ targetVersion }` ] );
+			// await run( 'git', [ 'push', 'origin', 'master' `--tags` ] );
+		}else{
+			console.log( `Dry run finished, running git diff to see package changes...` );
+			// const { stdout } = await run( 'git', [ 'diff' ], { stdio: 'pipe' } );
+			// console.log( stdout );
+		}
+	}
+}
+
+main( args )
+.catch( err => {
+	if( versionUpdated ){// revert to current version on failed releases
+		updatePackageVersion( currentVersion );
+	}
+	console.error( err );
+	process.exit( 1 );
+} );
+
+//___
+
+async function publish ( flags ){
+	try {
+		const { stdout } = await run('npm',[
+				'publish',
+				// ...( releaseTag ? [ '--tag', releaseTag ] : [] ),
+				'--access=public',
+				...flags,
+			],
+			{
+				// cwd: getPkgRoot( pkgName ),
+				stdio: 'pipe',
+			},
+		)
+		console.log( stdout );
+		console.log( pico.green( `Package successfully published` ) );
+	} catch(/** @type {any} */ e ) {
+		if( e.stderr.match( /previously published/ ) ){
+			console.log( pico.red( `Package already published` ) );
+		} else {
+			throw e;
+		}
+	}
+}
+
+function updatePackageVersion ( version ){
+	const pkgPath = path.resolve( __dirname, '../package.json' );
+	/** @type {Package} */
+	const pkg = JSON.parse( fs.readFileSync( pkgPath, 'utf-8' ) );
+	pkg.version = version;
+	fs.writeFileSync( pkgPath, JSON.stringify( pkg, null, 2 ) + '\n' );
+}
+
+function inc(/** @type {import('semver').ReleaseType} */ i ){
+	return semver.inc( currentVersion, i, preId );
+}
+
+async function run (
+	/** @type {string} */ bin,
+	/** @type {ReadonlyArray<string>} */ args,
+	/** @type {import('execa').Options} */ opts = {},
+){
+	return execa( bin, args, { stdio: 'inherit', ...opts } );
+}
+
+function step(/** @type {string} */ msg ){
+	return console.log( pico.green( '»' ), msg );
+}

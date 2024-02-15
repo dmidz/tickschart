12ca9a2f01b22e8213d6e2abfd9aa071be7daa40
David MARTIN <david@dmidz.com>
2024-02-15 14:02:13 +0100

    add demo/vanilla-js

--- demo/vanilla-js/main.js
+++ demo/vanilla-js/main.js
@@ -1,16 +1,15 @@
 // ! change this import to @dmidz/tickschart
-import { Chart, Fetcher, defaultTick, intervalsMs, type CandleTick } from '../../dist';
+import { Chart, Fetcher, defaultTick, intervalsMs } from '../../dist';
 import './style.css';
 import { ref } from 'vue';
 
 //__
 const { m1, h1, d1 } = intervalsMs;
-type DataTick = Record<string, CandleTick>;
 
 //_____ settings
 // ! adapt this to your public sample path
 const sampleTicksURL = `${ window.location.origin }/data/ticks_BTC_4h/1692000000000.json`;
-let sampleTicks: DataTick | null = null;
+let sampleTicks = null;
 const timeScaleMs = h1 * 4;
 const ticksPerLoad = 500;
 const rangeLoadMs = ticksPerLoad * timeScaleMs;
@@ -18,7 +17,7 @@ const xOriginRatio = .75;
 const currentTime = ref( new Date( Date.UTC( 2023, 9, 10 ) ) );
 
 //__
-const fetcher = new Fetcher<CandleTick, DataTick>( defaultTick, async ( startTime, limit ) => {
+const fetcher = new Fetcher( defaultTick, async ( startTime, limit ) => {
 	/*__ this example uses a unique local json file ( 500 ticks ) served in dev mode, replace this by an API call
 	 with passed params such startTime & limit + other such symbol & timeScale string ( 15m / 4h / d1... ) */
 
@@ -65,7 +64,7 @@ const dateFormatCrossHair = new Intl.DateTimeFormat( undefined, {
 	weekday: 'short', year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit',
 } );
 
-const chart = new Chart( document.getElementById('chart'), timeScaleMs, ( index: number ) => {
+const chart = new Chart( document.getElementById('chart'), timeScaleMs, ( index ) => {
 	/*__ one would normally pass fetcher.getTick directly, but for the only one file sample
 				we can bypass it to always return a tick from the file ( 1692000000000 ) time range */
 	return fetcher.getMapTicks( index )?.[ 1692000000000 + index % rangeLoadMs ] || defaultTick;
@@ -81,7 +80,7 @@ const chart = new Chart( document.getElementById('chart'), timeScaleMs, ( index:
 	},
 	uiScaleX: {
 		stepsRange: [ m1, m1 * 5, m1 * 10, m1 * 15, m1 * 30, h1, h1 * 1.5, h1 * 3, h1 * 6, h1 * 12, d1 ],
-		formatLabel: ( value: number ): string => {
+		formatLabel: ( value ) => {
 			const d = new Date( value );
 			if ( !( value % d1 ) ){
 				return `${ d.getUTCDate() }`;

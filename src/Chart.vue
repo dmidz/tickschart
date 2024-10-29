232ff38fa726432f8d3251529ca5e26036f3f71f
David MARTIN <david@dmidz.com>
2024-10-29 16:58:27 +0100

    add env API_URL and SAMPLE_MODE to switch in sample or real api mode

--- src/Chart.vue
+++ src/Chart.vue
@@ -8,6 +8,8 @@ type Tick = typeof defaultTick;
 type DataTick = Record<string, Tick>;//__ structure of one ticks load
 
 //_____ main settings
+const API_URL = import.meta.env.VITE_API_URL;
+const SAMPLE_MODE = !API_URL;//__ either using unique json data  (true) or normal API mode ( false )
 const defaultTick = { time: 0, open: 0, high: 0, low: 0, close: 0, vol: 0 } as const;//__ define the structure of your ticks
 // chart works with 5 minimal tick properties: open, high, low, close & volume, if your API returns different format,
 //   adapt the map below to match these needed properties to your tick properties
@@ -15,12 +17,13 @@ const mapTickProps = { open: 'open', high: 'high', low: 'low', close: 'close', v
 // this example uses a unique local json file ( 1000 ticks ) served in dev mode, replace this by an API call
 //	 with passed params such startTime & limit + other such symbol & timeScale string ( 15m / 4h / d1... ) */
 const sampleTimeStart = 1684800000000;
-const ticksPerLoad = 1000;// must match the ticks count per fetch
-const ticksURL = `${ window.location.origin }/tickschart/data/ticks_BTC_4h/${ sampleTimeStart }-${ ticksPerLoad }.json`;
+const ticksPerLoad = SAMPLE_MODE ? 1000 : 500;// must match the ticks count per fetch
+const ticksURL = SAMPLE_MODE
+	? `${ window.location.origin }/tickschart/data/ticks_BTC_4h/${ sampleTimeStart }-${ ticksPerLoad }.json`
+	: API_URL;
 const timeScaleMs = h1 * 4;// must match time scale of fetched data ( here 4h )
-const currentTime = new Date();// initial time position
-// const currentTime = new Date( Date.UTC(2023, 6, 13 ) );// initial time position
-// const currentTime = new Date( Date.UTC(2023, 7, 4 ) );// initial time position
+// const currentTime = new Date();// initial time position
+const currentTime = new Date( Date.UTC( 2023, 10, 9 ) );
 const xOriginRatio = .75;// screen width delta ratio, .75 = 3/4 width from left 
 const dateFormatCrossHair = new Intl.DateTimeFormat( undefined, { 
 	timeZone: 'UTC',
@@ -36,15 +39,16 @@ let chart: Chart<Tick>;
 // let player: Player<Tick>;
 
 const fetcher = new Fetcher( defaultTick, async ( startTime, limit ) => {
-	if( sampleTicks ){ return sampleTicks;}
+	if( SAMPLE_MODE && sampleTicks ){ return sampleTicks;}
 
 	const url = new URL( ticksURL );
 	
-	url.search = new URLSearchParams( {// sample of params for API ( useless with local json fetch ) 
+	url.search = new URLSearchParams( {// sample of params for API ( useless when SAMPLE_MODE ) 
 		symbol: 'BTCUSDT',
 		interval: '4h',// if API requires interval as a string choice, of course it must corresponds to timeScaleMs value
 		startTime: `${startTime}`,
 		limit: `${limit}`,
+		token: import.meta.env.VITE_API_TOKEN,
 	} ).toString();
 	
 	const response = await fetch( new Request( url, {  method: 'GET' } ) );
@@ -77,11 +81,11 @@ onMounted( async () => {
 	if( !refChartWrapper.value ){ return;}
 	let init = false;//__ using to prevent fetching until chart fully initialized ( timeScale, symbol, etc )
 	
-	chart = new Chart<Tick>( refChartWrapper.value, timeScaleMs, ( index: number ) => {
-		/*__ one would normally pass fetcher.getTick directly, but for the only one file sample
-					we can bypass it to always return a tick from the file ( 1692000000000 ) time range */
+	chart = new Chart<Tick>( refChartWrapper.value, timeScaleMs, SAMPLE_MODE ? ( index: number ) => {
+		/*__ normally pass fetcher.getTick directly, but when SAMPLE_MODE
+					we can bypass it to always return a tick from the file time range */
 		return fetcher.getMapTicks( index )?.[ sampleTimeStart + index % rangeLoadMs ] || defaultTick;
-	}, {
+	} : fetcher.getTick, {
 		mapTickProps,
 		onScalingXChange: async ( scalingX ) => {
 			if( !init ){  return;}//__ avoid any fetch during initialization
@@ -112,11 +116,11 @@ onMounted( async () => {
 		// }
 	} );
 	
-	chart.addIndicator( 'Volume', 'row', { maLength: 14, maType: 'sma' } );
+	// chart.addIndicator( 'Volume', 'row', { maLength: 14, maType: 'sma' } );
 	// chart.addIndicator( 'VolumeImpulse', 'row', { maLength: 14, maType: 'sma' } );
-	chart.addIndicator( 'OBV', 'row' );
+	// chart.addIndicator( 'OBV', 'row' );
 	// chart.addIndicator( 'MA', 'layer', { property: 'close', length: 50, type: 'ema', style: { color: '#ffff00'} } );
-	chart.addIndicator( 'MA', 'layer', { property: 'close', length: 200, type: 'sma', style: { color: '#ff0000'} } );
+	// chart.addIndicator( 'MA', 'layer', { property: 'close', length: 200, type: 'sma', style: { color: '#ff0000'} } );
 	// chart.addIndicator( 'MA', 'layer', { property: 'close', length: 100, type: 'sma', style: { color: '#ffff00'} } );
 	// chart.addIndicator( 'MA', 'layer', { property: 'close', length: 50, type: 'sma' } );
 	// chart.addIndicator( 'MA', 'layer', { property: 'close', length: 21, type: 'sma' } );
